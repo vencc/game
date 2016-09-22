e1962a906deced5b4f4330b87e0a38ac13f8961e
zpsuse <470360326@qq.com>
2016-09-22 21:33:06 +0800

    修改规则

--- src/util/IChess.java
+++ src/util/IChess.java
@@ -18,7 +18,7 @@ public interface IChess {
    * 功能: 悔棋时删除棋盘上的棋子
    * 作者: 黄欢欢   时间: 2016-09-20
    * @param x  横坐标
-   * @param y  纵坐标
+   * @param y  纵坐标·
    * @param type  0:白棋   1:黑棋
    */
   void delete(int x,int y,int type);
@@ -31,5 +31,6 @@ public interface IChess {
    * @param type  0:白棋   1:黑棋
    */
   boolean compare(int x,int y,int type);
+ 
 
 }
