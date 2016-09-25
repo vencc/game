c0011f67217d4e4f0938eb3716a1112ec998546f
vencc <2641150756@qq.com>
2016-09-25 21:44:08 +0800

    Revert "Develop"

--- src/msg/ClientLoginMsg.java
+++ src/msg/ClientLoginMsg.java
@@ -34,21 +34,10 @@ public class ClientLoginMsg extends BaseMsg{
 			MyServer.getMyServer().sendMsgToClient(msg, this.client);
 			//3.将客户端的用户名与所分配到的线程进行绑定
 			//有username查询dao得到User对象user
-<<<<<<< HEAD
-			
-			MyServer.getMyServer().bindUsername(user, client);
-			//4.向所有客户端发送在线用户列表
-			ServerUserListMsg msg2=new ServerUserListMsg(MyServer.getMyServer().getUserList());
-			MyServer.getMyServer().sendMsgToAll(msg2);
-			//5.服务器向登录的客户端发送房间列表报文
-			ServerRoomListMsg msg3=new ServerRoomListMsg(MyServer.getMyServer().getRooms());
-			MyServer.getMyServer().sendMsgToClient(msg3, this.client);
-=======
 			UserDao dao=new UserDao();
 			User user=dao.aa();
 			MyServer.getMyServer().bindUsername(user, client);
 			//4.向客户端发送在线用户列表
->>>>>>> 84dff651532ad0cd83bf24e1cca02f2077115b3d
 			
 			
 		}else{
